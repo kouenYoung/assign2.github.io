8705f6802224938d10b9d2df8b227a0961da74a7
Noah Hartzfeld <nhartzfeld@wesleyan.edu>
2022-03-01 18:20:52 -0500

    registration

--- index.php
+++ index.php
@@ -9,6 +9,16 @@
     <body>
         <p>This is our first PHP and SQL example of a dynamically served 
             web page!</p>
+
+
+<form method="POST" action="">
+Username: <input type="text" name="username1" /><br>
+Password: <input type="text" name="password" /><br>
+<input type="submit" name="submit" value="Register"/><br>
+  </form>
+
+
+
     <?php
         $servername = "localhost";
         $username = "root";
@@ -20,13 +30,46 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        echo "Connected successfully";
+        echo "Connected successfully <br><br>";
 
+        if(isset($_REQUEST["submit"])){
+            $out_value = "";
+            $username = $_REQUEST['username1'];
+            $password = $_REQUEST['password'];
+            if(!empty($username) && !empty($password)){
+                $sql_query = "SELECT * FROM users WHERE username = ('$username')";
+               $result = mysqli_query($conn, $sql_query);
+               $row = mysqli_fetch_assoc($result);
+
+
+               if($row != 0){
+                   echo "You already have an account<br><br>";
+               }
+               else {
+                $sql_query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+                mysqli_query($conn, $sql_query);
+               
+                echo "User succesfully added!<br><br>";
+
+               }
+        
+
+            }
+
+            else {
+                echo "Please enter Username and Password";
+            }
+
+
+
+
+        }
+        
 
 
         if(isset($_REQUEST["submit"])){
             $out_value = "";
-            $username = $_REQUEST['username'];
+            $username = $_REQUEST['username2'];
 
             if(!empty($username)){
                 $sql_query = "SELECT * FROM ratings WHERE username = ('$username')";
@@ -44,7 +87,7 @@
 
             }
             else {
-                $out_value = "No rating available!";
+                $out_value = "No grade available!";
             }
         }
 
@@ -52,8 +95,10 @@
         ?>
 
 <form method="POST" action="">
-  Username: <input type="text" name="username" placeholder="Username:" /><br>
-  <input type="submit" name="submit" value="Submit"/>
+<hd3> Retrieve Songs by Username </hd3><br>
+Username: <input type="text" name="username2" /><br>
+
+  <input type="submit" name="submit" value="Retrieve"/>
   </form>
   
   <p><?php 
