1e41d0fa2a201feabb4f254e6eba4193601b92d2
Luke Morrill <lukemorrill@icloud.com>
2022-03-05 00:41:06 -0500

    created django app, set up models

---
+++ django-app/music_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=200, primary_key=True)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+class Artists(models.Model):
+    song = models.CharField(max_length=200, primary_key=True)
+    artist = models.CharField(max_length=200)
+
+class Ratings(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.ForeignKey(Users, to_field="username", on_delete=models.CASCADE)
+    song = models.CharField(max_length=200)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.username
---
+++ django-app/readme.md
@@ -0,0 +1,14 @@
+This is the instructions on how to run this Django App
+
+from within the django-app directory, run the music_app
+with
+
+```shell
+python3 -m venv venv
+source venv/bin/activate
+python3 manage.py runserver
+```
+
+Then go to:
+
+INSERT FINAL INDEX PAGE HERE
