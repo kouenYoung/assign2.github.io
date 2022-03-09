d9c2cab4644fcbc60951bc505db01b46efc0397c
Noah Hartzfeld <nhartzfeld@wesleyan.edu>
2022-03-08 23:29:06 -0500

    print-usernames

--- django-app/music_app/urls.py
+++ django-app/music_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .import views
 from django.contrib import admin
 
+from music_app.views import register
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('users/', views.index, name="list-usernames")
+    path('users/', views.index, name="list-usernames"),
+    path('register/', views.register, name = "register")
+
 ]
--- django-app/music_app/views.py
+++ django-app/music_app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import UsersForm
+
 from .models import Users
 
 # Create your views here.
 
 def index(request):
-    users = Users.objects.all().order_by('username')
+    users = Users.objects.all()
     return render(request, 'music_app/index.html', {'users': users})
+
+
+def register(request):
+    form = UsersForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+        context = {
+            'form': form
+        }
+        return render(request, 'music_app/register.html', context)
