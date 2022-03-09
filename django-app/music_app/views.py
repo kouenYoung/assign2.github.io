2a1b4a988348e0a0a0fec8dfbfea6cde8ea84a5e
Noah Hartzfeld <nhartzfeld@wesleyan.edu>
2022-03-08 21:57:00 -0500

    failed-table-access

--- django-app/music_app/views.py
+++ django-app/music_app/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .forms import UsersForm
-
-def users_create_view(request):
-    form = UsersForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-
-        context = {
-            'form': form
-        }
-        return render(request, "music_app/index.html", context)
+from .models import Users
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Placeholder for future interface")
-
-def users_detail_view(request):
-    obj = Product.objects.get(id=1)
-    return render(request, "user/detail.html", {})
+    users = Users.objects.all().order_by('username')
+    return render(request, 'music_app/index.html', {'users': users})
